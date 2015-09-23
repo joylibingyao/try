5095581f73b562958e5a25cd59c3d7125d98889c
joylibingyao <joylibingyao@gmail.com>
2015-09-23 01:57:43 -0400

    try to fix open issue

---
+++ server.js
@@ -0,0 +1,18 @@
+
+var express = require('express');
+
+var path = require('path');
+
+var app = express();
+
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, './client')));
+
+app.listen(8008, function() {
+  console.log('cool stuff on: 8008');
+});
